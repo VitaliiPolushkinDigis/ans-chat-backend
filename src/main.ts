ab9d8cf199e725ae3c95246713ff46b8b1c9ead1
VitaliiPolushkinDigis <91121101+VitaliiPolushkinDigis@users.noreply.github.com>
2022-09-10 11:04:38 +0300

    initial setup and create auth folders

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const { PORT } = process.env;
+
+  await app.listen(PORT, () =>
+    console.log(`-------App is running on http://localhost:${PORT}`),
+  );
+}
+bootstrap();
