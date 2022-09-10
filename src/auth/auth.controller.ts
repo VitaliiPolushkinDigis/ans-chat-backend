d621fdb75d64943b56d41d2b07596967b5274b15
VitaliiPolushkinDigis <91121101+VitaliiPolushkinDigis@users.noreply.github.com>
2022-09-10 11:29:48 +0300

    add validation for register dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,31 @@
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Routes, Services } from 'src/utils/types';
 import { IAuthService } from './auth';
+import { CreateUserDto } from './dtos/CreateUser.dto';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(@Inject(Services.AUTH) private authService: IAuthService) {}
+
+  @Post('register')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+
+  /*   @Post('login')
+  loginUser() {}
+
+  @Get('status')
+  status() {}
+
+  @Post('logout')
+  logout() {} */
 }
