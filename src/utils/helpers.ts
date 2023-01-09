37aa7976d833ba67af83b26e3a75fd96f0a0efe4
dione616 <Vetal160199@i.ua>
2023-01-09 04:21:33 +0200

    deploy to aws with bcryptjs

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const bcrypt = require('bcrypt');
+// eslint-disable-next-line no-var
+var bcrypt = require('bcryptjs');
 
 export const hashPassword = async (rawPassword: string) => {
   const salt = await bcrypt.genSalt(10);
