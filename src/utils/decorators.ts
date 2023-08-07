5da619e0f715a5ceda96ed525c17387237c4443a
VitaliiPolushkinDigis <91121101+VitaliiPolushkinDigis@users.noreply.github.com>
2023-08-07 21:34:39 +0300

    undo guards

--- src/utils/decorators.ts
+++ src/utils/decorators.ts
@@ -4,8 +4,6 @@ import { AuthenticatedRequest } from './types';
 export const AuthUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = <AuthenticatedRequest>ctx.switchToHttp().getRequest();
-    console.log('---request', request);
-
     return request.user;
   },
 );
